75c5c75d91b49e08da2a87bdc6f4b8a71790eb9a
17BarringerJosiah <59709224+17BarringerJosiah@users.noreply.github.com>
2023-11-27 19:41:02 -0500

    Add files via upload

---
+++ demonstrateInheritance/src/demonstrateInheritance/CommissionEmployee.java
@@ -0,0 +1,61 @@
+//<CommissionEmployee> -- Set Commission
+//<CSIS312><D01>
+
+package demonstrateInheritance;
+
+//Fig. 9.10: CommissionEmployee.java
+//CommissionEmployee class uses methods to manipulate its 
+//private instance variables.
+
+public class CommissionEmployee extends Employee {
+    private double grossSales;
+    private double commissionRate;
+
+    public CommissionEmployee(String firstName, String lastName, String socialSecurityNumber,
+                              double grossSales, double commissionRate) {
+        super(firstName, lastName, socialSecurityNumber);
+
+        if (grossSales < 0.0)
+            throw new IllegalArgumentException("Gross sales must be >= 0.0");
+
+        if (commissionRate <= 0.0 || commissionRate >= 1.0)
+            throw new IllegalArgumentException("Commission rate must be > 0.0 and < 1.0");
+
+        this.grossSales = grossSales;
+        this.commissionRate = commissionRate;
+    }
+
+    public void setGrossSales(double grossSales) {
+        if (grossSales < 0.0)
+            throw new IllegalArgumentException("Gross sales must be >= 0.0");
+
+        this.grossSales = grossSales;
+    }
+
+    public double getGrossSales() {
+        return grossSales;
+    }
+
+    public void setCommissionRate(double commissionRate) {
+        if (commissionRate <= 0.0 || commissionRate >= 1.0)
+            throw new IllegalArgumentException("Commission rate must be > 0.0 and < 1.0");
+
+        this.commissionRate = commissionRate;
+    }
+
+    public double getCommissionRate() {
+        return commissionRate;
+    }
+
+    public double earnings() {
+        return getCommissionRate() * getGrossSales();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s%n%s: %.2f", "commission",
+                super.toString(),
+                "gross sales", getGrossSales(),
+                "commission rate", getCommissionRate());
+    }
+}
